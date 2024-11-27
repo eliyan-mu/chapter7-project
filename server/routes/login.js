ec47c6aa674b276dc0d65ee0b320a7c73066c31d
rivka <rivkamiller@hilma.tech>
2024-11-27 18:37:22 +0200

    gets id of uername provided if exsts

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,20 +9,28 @@ var con = mysql.createConnection({
   database: "project7database",
 });
 
-router.get(`/`, function (req, res, next) {
+router.post(`/`, function (req, res, next) {
   con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
   });
 
-  var sql = `SELECT id FROM user WHERE username = ${req.body.username}`;
-  con.query(sql, function (err, result) {
+  const { username } = req.body;
+
+  if (!username) {
+    return res.status(400).send("username is required");
+  }
+
+  var sql = `SELECT id FROM user WHERE username = ?`;
+  con.query(sql, [username], function (err, result) {
     console.log("result: ", result);
     if (err) throw err;
-    return res.status(200).send(`query result is ${result}`);
+    if (result.length > 0) {
+      return res.status(200).send(result[0].id);
+    } else {
+      return res.status(404).send("User not found");
+    }
   });
-  // res.json(res);
-  res.send("not successfll login");
 });
 
 module.exports = router;
