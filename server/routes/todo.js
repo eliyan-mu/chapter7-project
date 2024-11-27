f3b697abac42434f6e99b2f6e8fccf7fbe282b6a
rivka <rivkamiller@hilma.tech>
2024-11-27 14:54:21 +0200

    add serverside get all todos of user

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,9 +1,30 @@
 var express = require("express");
 var router = express.Router();
+var mysql = require("mysql");
 
-/* GET home page. */
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "z10mz10m",
+  database: "project7database",
+});
+
 router.get("/", function (req, res, next) {
   res.send("entered todo route");
+});
+
+/* GET ALL TODOS. */
+router.get(`/:user_id`, function (req, res, next) {
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  var sql = `SELECT * FROM todo WHERE user_id = ${Number(req.params.user_id)}`;
+  return con.query(sql, function (err, result) {
+    if (err) throw err;
+
+    return res.status(200).send(result);
+  });
 });
 
 module.exports = router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+let user_id = 3;
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("entered user route");
+});
+
+router.get(`/${user_id}`, function (req, res, next) {
+  res.send(`entered user route on user_id = ${user_id}`);
 });
 
 module.exports = router;
