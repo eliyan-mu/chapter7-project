2cce495d6c7551aee15f7ef3305ecb4b1b7ccbeb
eliyan-mu <eliyanmousazadeh@hilma.tech>
2024-11-27 19:13:01 +0200

    client-router

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate, NavLink, Outlet } from "react-router-dom";
+// import {fetchItems} from "../RequestUtils";
+function Home() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/");
+  };
+  return (
+    <>
+      <p>Hello!!</p>
+      <div style={{ marginTop: "-200    px" }}>
+        <button onClick={handleLogout}>log-out</button>
+        <NavLink to="/home">
+          <button>Home</button>
+        </NavLink>
+        <NavLink to="todo">
+          <button>To-Do</button>
+        </NavLink>
+        <NavLink to="post">
+          <button>Posts</button>
+        </NavLink>
+      </div>
+      <Outlet />
+    </>
+  );
+}
+
+export default Home;
