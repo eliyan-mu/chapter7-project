27bda674873d39bec7ead5df09abdafba86cfb16
rivka <rivkamiller@hilma.tech>
2024-11-27 13:50:18 +0200

    add start for users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "z10mz10m",
+  database: "project7database",
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("successfll login");
+});
+
+// get name of user
+router.get("/3", function (req, res) {
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  var sql = `SELECT users_name FROM user WHERE user_id=3`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    return res.status(200).send(`You've successfully deleted the student`);
+  });
+});
+
+// get todos of users
+router.get("/user_id/todos", function (req, res) {
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  var sql = `SELECT * FROM todos WHERE user_id=3`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    return res.status(200).send(`You've successfully deleted the student`);
+  });
 });
 
 module.exports = router;
